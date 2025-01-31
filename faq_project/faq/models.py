8a825757897711dfb4ca02ac335f9f70dbbde924
Aryan Chhetri <aryanjr53@gmail.com>
2025-01-31 18:05:48 +0530

    feat add FAQ model

--- faq_project/faq/models.py
+++ faq_project/faq/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.core.cache import cache
+from googletrans import Translator
 
-# Create your models here.
+translator = Translator()
+
+class FAQ(models.Model):
+    question = models.TextField
+    answer = RichTextField()
+    
+    def get_translation(self, lang='en'):
+        cache_key = f'faq_{self.id}_{lang}'
+        cached_translation = cache.get(cache_key)
+
+        if cached_translation:
+            return cached_translation
+        
+        translated_text = translator.translate(self.question, dest=lang).text
+
+        cache.set(cache_key, translated_text, timeout= 3000)
+        return translated_text
+    
+    def __str__(self):
+        return self.question
