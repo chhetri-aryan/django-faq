b9a273be40d6cfc73dbe5532cee1a77cf960c863
Aryan Chhetri <aryanjr53@gmail.com>
2025-02-01 13:27:44 +0530

    Implementing caching mechanism with Redis.

--- faq_project/faq_project/settings.py
+++ faq_project/faq_project/settings.py
@@ -136,3 +136,14 @@ CKEDITOR_CONFIGS = {
         'width' : '100%'
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',  # Use Redis server
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
